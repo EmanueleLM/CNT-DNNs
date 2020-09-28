e0c2e7d32e0f18b1478523189a3443def757ed25
Emanuele <emanuele.lamalfa@mail.polimi.it>
2020-09-28 13:20:11 +0200

    Experiments file now is modular

--- run_exp.sh
+++ run_exp.sh
@@ -9,7 +9,7 @@
 # Example sh ./run_exp.sh MNIST fc 1.0 0.025 0.05 1000 0.5 0.9
 for i in {0..9}
 do
-    let seed="i*1000"
+    #let seed="i*1000"
     echo 'python3 train_vision.py -d $1 -a $2 --cut-train $3 --seed $seed --bins $4 --scale $5 --sims $6 --min $7 --max $8'
-    screen -d -m nice bash -c 'python3 train_vision.py -d $1 -a $2 --cut-train $3 --seed $seed --bins $4 --scale $5 --sims $6 --min $7 --max $8'
+    #screen -d -m nice bash -c 'python3 train_vision.py -d $1 -a $2 --cut-train $3 --seed $seed --bins $4 --scale $5 --sims $6 --min $7 --max $8'
 done
