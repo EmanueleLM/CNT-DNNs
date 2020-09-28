d3031dc1e10621b080aa4b9ed9bd661c488a9a9e
Emanuele <emanuele.lamalfa@mail.polimi.it>
2020-09-28 12:08:41 +0200

    sh file to monitor training.

--- cnt_nets.sh
+++ cnt_nets.sh
@@ -3,5 +3,5 @@
 # arg $1 is the dataset (MNIST, CIFAR10 etc.), arg $2 is the support (0.05, 0.5, 5.0)
 # Example of usage:  sh ./cnt_nets.sh MNIST 0.05
 for i in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
-do ls ./weights/$1/ | grep support-$2 | grep binaccuracy-$i |wc -l
+do echo "Accuracy $i: num. nets: "; ls ./weights/$1/ | grep support-$2 | grep binaccuracy-$i |wc -l
 done
