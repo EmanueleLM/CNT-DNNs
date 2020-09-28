9a1b3d4614775e24d177cef0e6e7beb89dea61ba
Emanuele <emanuele.lamalfa@mail.polimi.it>
2020-09-28 12:34:57 +0200

    Experiments file now is modular

--- monitor_results.sh
+++ monitor_results.sh
@@ -1,2 +1,3 @@
+# old
 cd ./results/fc
 du -a | cut -d/ -f2 | sort | uniq -c | sort -nr
---
+++ run_exp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# $1: dataset (MNIST, CIFAR10)
+# $2: architecture (fc, cnn, rnn, attention)
+# $3: cut-train (from 0.0 to 1.0)
+# $4: bins (0.025, usually)
+# $5: scale (0.05, 0.5, 5.0)
+# $6: sims (1000 usually)
+# $7, $8: min-max values of accuracy considered (all the other are discarded)
+# Example run_exp.sh MNIST fc 1.0 0.025 0.05 1000 0.5 0.9
+for i in 0 1 2 3 4 5 6 7 8 9
+seed = ($i*$6)
+do screen -d -m nice bash -c 'python3 train_vision.py -d $1 -a $2 --cut-train $3 --seed $seed --bins $4 --scale $5 --sims $6 --min $7 --max $8'
+done
