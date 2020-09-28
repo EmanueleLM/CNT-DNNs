fc563581e1d51accaa3a6cfe3e67ee10ae4b9337
Emanuele <emanuele.lamalfa@mail.polimi.it>
2020-09-28 19:18:38 +0200

    Nodes strength and fluctuation

--- ComplexNetwork.py
+++ ComplexNetwork.py
@@ -26,11 +26,27 @@ class __Network(object):
         
 class __FCLayer(__Network):
     def __init__(self, architecture, num_layers, weights, input_size, output_size, flatten=True):
-        self.super
+        super(__FCLayer, self).__init__()
         self.architecture = 'fc'
         self.weights = []
         self.biases = []
+        # Collect weights and biases for each layer
         for i in range(0, 2*num_layers, 2):
             self.weights.append((weights[i].flatten() if flatten else weights[i]))
             self.biases.append((weights[i+1].flatten() if flatten else weights[i+1]))
             
+        def nodes_strength(self, layer):
+            if layer == 0:
+                strength = self.weights[0].sum(axis=-1) + 1
+            elif layer == self.num_layers:
+                strength = self.weights[-1].sum(axis=0) + 1 + self.biases[-1]
+            else:
+                s_in = self.weights[layer].sum(axis=-1) 
+                s_out = self.weights[layer-1].sum(axis=0) + self.biases[layer-1]
+                strength = s_in + s_out
+            return strength
+        
+        def nodes_fluctuation(self, layer):
+            fluctuation = self.nodes_strength(layer).std()
+            return fluctuation   
+        
