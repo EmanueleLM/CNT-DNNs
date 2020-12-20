0a2c0060ed54c4e8b6815684b11aa977f49d18c2
emanuele.lamalfa@mail.polimi.it <emanuele.lamalfa@mail.polimi.it>
2020-12-20 17:16:35 +0000

    plot generator for a population of cnns.

--- plot_generator_cnn.py
+++ plot_generator_cnn.py
@@ -35,7 +35,7 @@ parser.add_argument("-a", "--architecture", dest="architecture", default='cnn', 
                     help="Architecture (fc or cnn so far).")
 parser.add_argument("-d", "--dataset", dest="dataset", default='MNIST', type=str,
                     help="Dataset prefix used to save weights (MNIST, CIFAR10).")
-parser.add_argument("-l", "--layers", dest="num_layers", default=5, type=int,
+parser.add_argument("-l", "--layers", dest="num_layers", default=4, type=int,
                     help="Number of layers of the models considered.")
 parser.add_argument("-b", "--bins", dest="bins_size", default=0.1, type=float,
                     help="Accuracy range per-bin.") 
@@ -65,9 +65,8 @@ num_std_dev = float(args.num_std_dev)
 if len(init)>0 and '*' not in init:
     print("[logger-WARNING]: Your init technique might require a * at the end, otherwise the filtering will be strict on the patter --init={}".format(init))
 
-num_weights = num_layers-1  # a two layers nn has one set of parameters ;)
 ranges_accuracy = np.arange(0., 1.0, bins_size)
-input_shape, output_size = ((1,28,28) if dataset=='MNIST' else (32,32,3)), 10
+input_shape, output_size = ((28,28,1) if dataset=='MNIST' else (32,32,3)), 10
 num_conv_layers = 2
 paddings, strides = (0,0), (1,1)
 init = ('*' if len(init)==0 else init)
@@ -84,8 +83,8 @@ colors = list(red.range_to(Color("red"), num_colors))
 # Link weights
 layers_link_weights = {}
 link_weights_single_layer = {k:v for (k,v) in zip(['{:4.4f}'.format(r) for r in ranges_accuracy], [np.array([]) for _ in range(num_colors)])}
-link_weights = [cp.copy(link_weights_single_layer) for _ in range(num_weights)]  # one dictionary per layer
-link_weights_densities = [[] for _ in range(num_weights)]  # Collect densities (to estimate divergence between )
+link_weights = [cp.copy(link_weights_single_layer) for _ in range(num_layers)]  # one dictionary per layer
+link_weights_densities = [[] for _ in range(num_layers)]  # Collect densities (to estimate divergence between )
 print("\n[logger]: Generating weights histogram PDFs and error-bars")
 for i, acc in enumerate(ranges_accuracy):
     acc_prefix = "{:4.4f}".format(acc)
@@ -103,7 +102,7 @@ for i, acc in enumerate(ranges_accuracy):
         if  np.any([np.isnan(w).any() for w in W]):
             continue
         CNet = ComplexNetwork(architecture, num_layers, num_conv_layers, W, input_shape, output_size, strides=strides, paddings=paddings, flatten=False)
-        for l in range(num_weights):
+        for l in range(num_layers):
             link_weights[l][acc_prefix] = np.concatenate((link_weights[l][acc_prefix], CNet.weights[l].flatten(), CNet.biases[l]))
         if processed_files >= maxfiles:
             break
@@ -111,12 +110,12 @@ for i, acc in enumerate(ranges_accuracy):
 # Filter data
 if num_std_dev > 0.:
     print("[logger]: Link Weights are filtered of {} std.".format(num_std_dev))
-    for l in range(num_weights):
+    for l in range(num_layers):
         for i, acc in enumerate(ranges_accuracy):
             if len(link_weights[l][acc_prefix]) != 0:
                 acc_prefix = "{:4.4f}".format(acc)
                 link_weights[l][acc_prefix] = reject_outliers(link_weights[l][acc_prefix], num_std_dev)
-for l in range(num_weights):
+for l in range(num_layers):
     print("[logger]: Generating plot for layer {}".format(l))
     for i, acc in enumerate(ranges_accuracy):
         acc_prefix = "{:4.4f}".format(acc)
@@ -138,8 +137,8 @@ for l in range(num_weights):
 
 # Nodes strength
 nodes_strength_single_layer = {k:v for (k,v) in zip(['{:4.4f}'.format(r) for r in ranges_accuracy], [np.array([]) for _ in range(num_colors)])}
-nodes_strength = [cp.copy(nodes_strength_single_layer) for _ in range(num_layers)]  # one dictionary per layer
-nodes_strength_densities = [[] for _ in range(num_layers)]  # Collect densities (to estimate divergence between )
+nodes_strength = [cp.copy(nodes_strength_single_layer) for _ in range(num_layers+1)]  # one dictionary per layer
+nodes_strength_densities = [[] for _ in range(num_layers+1)]  # Collect densities (to estimate divergence between )
 print("\n[logger]: Generating strengths histogram PDFs and error-bars")
 for i, acc in enumerate(ranges_accuracy):
     acc_prefix = "{:4.4f}".format(acc)
@@ -157,20 +156,20 @@ for i, acc in enumerate(ranges_accuracy):
         if  np.any([np.isnan(w).any() for w in W]):
             continue
         CNet = ComplexNetwork(architecture, num_layers, num_conv_layers, W, input_shape, output_size, strides=strides, paddings=paddings, flatten=False)
-        for l in range(num_layers):
-            nodes_strength[l][acc_prefix] = np.concatenate((nodes_strength[l][acc_prefix], CNet.nodes_strength(l)))
+        for l in range(num_layers+1):
+            nodes_strength[l][acc_prefix] = np.concatenate((nodes_strength[l][acc_prefix], CNet.nodes_strength(l).flatten()))
         if processed_files >= maxfiles:
             break
         idx_glob += 1
 # Filter data
 if num_std_dev > 0.:
     print("[logger]: Strenghts are filtered of {} std.".format(num_std_dev))
-    for l in range(num_layers):
+    for l in range(num_layers+1):
         for i, acc in enumerate(ranges_accuracy):
             if len(nodes_strength[l][acc_prefix]) != 0:
                 acc_prefix = "{:4.4f}".format(acc)
                 nodes_strength[l][acc_prefix] = reject_outliers(nodes_strength[l][acc_prefix], num_std_dev)
-for l in range(num_layers):
+for l in range(num_layers+1):
     print("[logger]: Generating plot for layer {}".format(l))
     for i, acc in enumerate(ranges_accuracy):
         acc_prefix = "{:4.4f}".format(acc)
@@ -192,7 +191,7 @@ for l in range(num_layers):
     
 # Nodes fluctuation
 link_nodes_fluctuation_single_layer = {k:v for (k,v) in zip(['{:4.4f}'.format(r) for r in ranges_accuracy], [np.array([]) for _ in range(num_colors)])}
-nodes_fluctuation= [cp.copy(link_nodes_fluctuation_single_layer) for _ in range(num_layers)]  # one dictionary per layer
+nodes_fluctuation= [cp.copy(link_nodes_fluctuation_single_layer) for _ in range(num_layers+1)]  # one dictionary per layer
 print("\n[logger]: Generating fluctuations histogram PDFs and error-bars")
 for i, acc in enumerate(ranges_accuracy):
     acc_prefix = "{:4.4f}".format(acc)
@@ -210,7 +209,7 @@ for i, acc in enumerate(ranges_accuracy):
         if  np.any([np.isnan(w).any() for w in W]):
             continue
         CNet = ComplexNetwork(architecture, num_layers, num_conv_layers, W, input_shape, output_size, strides=strides, paddings=paddings, flatten=False)
-        for l in range(num_layers):
+        for l in range(num_layers+1):
             nodes_fluctuation[l][acc_prefix] = np.concatenate((nodes_fluctuation[l][acc_prefix], CNet.nodes_fluctuation(l)))
         if processed_files >= maxfiles:
             break
@@ -218,12 +217,12 @@ for i, acc in enumerate(ranges_accuracy):
 # Filter data
 if num_std_dev > 0.:
     print("[logger]: Fluctuations are filtered of {} std.".format(num_std_dev))
-    for l in range(num_layers):
+    for l in range(num_layers+1):
         for i, acc in enumerate(ranges_accuracy):
             if len(nodes_fluctuation[l][acc_prefix]) != 0:
                 acc_prefix = "{:4.4f}".format(acc)
                 nodes_fluctuation[l][acc_prefix] = reject_outliers(nodes_fluctuation[l][acc_prefix], num_std_dev)
-for l in range(num_layers):
+for l in range(num_layers+1):
     print("[logger]: Generating plot for layer {}".format(l))
     for i, acc in enumerate(ranges_accuracy):
         acc_prefix = "{:4.4f}".format(acc)
@@ -241,9 +240,8 @@ for l in range(num_layers):
     #plt.show()
     plt.close()
 
-"""
 # Plot weights divergences
-for l in range(num_weights):
+for l in range(num_layers):
     num_ranges_accuracy = len(link_weights_densities[0])
     heatmap_link_weights = np.zeros(shape=(num_ranges_accuracy, num_ranges_accuracy))
     for i in range(num_ranges_accuracy):
@@ -266,7 +264,7 @@ for l in range(num_weights):
     np.save(saved_images_path + "{}_{}_{}_heatmap_link-weights_init-{}_support-{}_layer-{}{}".format(dataset, netsize, architecture, (init if init!='*' else 'any'), scaling_factor, l, '.npy'), heatmap_link_weights)
 
 # Plot nodes strength divergences
-for l in range(num_layers):
+for l in range(num_layers+1):
     num_ranges_accuracy = len(nodes_strength_densities[0])
     heatmap_nodes_strength = np.zeros(shape=(num_ranges_accuracy, num_ranges_accuracy))
     for i in range(num_ranges_accuracy):
@@ -287,4 +285,3 @@ for l in range(num_layers):
     plt.close()
     print("Shannon divergence (layer by layer) (Matrix)\n ", heatmap_nodes_strength)
     np.save(saved_images_path + "{}_{}_{}_heatmap_nodes-strength_init-{}_support-{}_layer-{}{}".format(dataset, netsize, architecture, (init if init!='*' else 'any'), scaling_factor, l, '.npy'), heatmap_nodes_strength)
-"""
